582a12a2d253decb2dc54126888c6a3ad63c21be
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-03-17 12:56:52 +0530

    Created pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+     <BrowserRouter>
+     <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/about' element={< About />} />
+          <Route path='/sign-in' element={< SignIn />} />
+          <Route path='/sign-out' element={< SignOut />} />
+          <Route path='/profile' element={< Profile />} />
+     </Routes>
+     
+     </BrowserRouter>
   );
 }
 
